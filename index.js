1ceec1a9fee2538eeb6d30cd3ab977c62e87fe55
krish406 <155498135+krish406@users.noreply.github.com>
2025-07-11 18:02:22 -0400

    addedcreation functionality

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 import express from "express";
 const app = express();
 const port = 3000;
+const blogs = [];
 
 app.use(express.static("public"));
+app.use(express.urlencoded({extended: true}));
 
 app.get("/", (req, res) => {
-  res.render("index.ejs")
+  res.render("index.ejs", {blogs: blogs});
+});
+
+app.post("/create", (req, res) => {
+  const blogObj = {
+    title: req.body["post-title"],
+    content: req.body["post-content"]
+  }
+  blogs.push(blogObj);
+  console.log(blogs)
+
+  res.redirect("/");
 });
 
 app.listen(port, () => {
--- public/script.js
+++ public/script.js
@@ -1,14 +1,11 @@
 const createButton = document.querySelector("#create-button");
-console.log(createButton);
 const createDialog = document.querySelector("#create-dialog");
+const closeDialog = document.querySelector("#close-dialog");
 
 createButton.addEventListener("click", () => {
-  console.log("heheh");
   createDialog.showModal();
 });
 
-// Close dialog functionality
-const closeDialog = document.querySelector("#close-dialog");
 closeDialog.addEventListener("click", () => {
   createDialog.close();
 });
