78749b4931914dc05fba91cd8ec9e375a9ee1d64
krish406 <155498135+krish406@users.noreply.github.com>
2025-07-12 10:38:47 -0400

    added read functionality

--- index.js
+++ index.js
@@ -13,13 +13,29 @@ app.get("/", (req, res) => {
 app.post("/create", (req, res) => {
   const blogObj = {
     title: req.body["post-title"],
-    content: req.body["post-content"]
+    content: req.body["post-content"],
+    id: crypto.randomUUID()
   }
+
   blogs.push(blogObj);
   console.log(blogs)
 
   res.redirect("/");
 });
+
+app.get("/read/:id", (req, res) => {
+  const userID = req.params.id;
+  const currentBlog = blogs.find((blog) => blog.id === userID);
+  console.log(`current Blog is ${currentBlog}`);
+
+  if(currentBlog && currentBlog.content){
+    console.log(currentBlog);
+    res.send(currentBlog.content);
+  }
+  else{
+    res.status(404).send("Blog not found");
+  }
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
